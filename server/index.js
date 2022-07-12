be513e90788992cbf6e6e7f59362de6fc2fc5e57
Parajdi Zoard <parajdi.zoard@gmail.com>
2022-07-12 18:57:13 +0300

    origin

--- server/index.js
+++ server/index.js
@@ -10,7 +10,9 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.all('/*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin",  req.header('Origin'));
+    res.header("Access-Control-Allow-Origin",  "https://cosmetic-ingredients-1.herokuapp.com");
+    //local 
+    // res.header("Access-Control-Allow-Origin",  "http://localhost:3000"); 
     res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type, Accept, Authorization");
     res.header("Access-Control-Allow-Methods", "POST, PUT, GET,DELETE,PATCH");
     res.header('Access-Control-Allow-Credentials', true);
